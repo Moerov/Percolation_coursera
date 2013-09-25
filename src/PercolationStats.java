3093f01b29508dae32f35c72052702520634cc8a
Moerov <moyorov@gmail.com>
2013-09-25 17:10:42 +0400

    It works!

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -9,49 +9,54 @@
 
 public class PercolationStats
 {
-    private int T;
-    private int N;
+    private int numberOfExperiments;
+    private int size;
     private double[] threshold;
     private double mean;
     private double stddev;
 
 
-    // perform T independent computational experiments on an N-by-N grid
+    // perform numberOfExperiments independent computational
+    // experiments on an size-by-size grid
     public PercolationStats(int N, int T)
     {
-        this.N = N;
-        //grid = new Percolation(N);
-        this.T = T;
-        threshold = new double[T];
+        check(N, T);
+        numberOfExperiments = T;
+        size = N;
+        threshold = new double[numberOfExperiments];
         mean = 0;
         stddev = 0;
         this.simulation();
     }
 
     private void simulation() {
-        for (int i = 0; i < T; i++) {
-            Percolation grid = new Percolation(N);
+        for (int i = 0; i < numberOfExperiments; i++) {
+            Percolation grid = new Percolation(size);
             int counter = 0;
             while (!grid.percolates()) {
-                int j = (int) (Math.floor(Math.random()*N + 1));
-                int k = (int) (Math.floor(Math.random()*N + 1));
+                int j = (int) (Math.floor(Math.random()* size + 1));
+                int k = (int) (Math.floor(Math.random()* size + 1));
                 if (!grid.isOpen(j, k)) {
                    grid.open(j, k);
                    counter++;
-                   //System.out.println("counter++");
-                }
+                   }
             }
-            threshold[i] = ((double) counter) / ((double) N*N);
+            threshold[i] = ((double) counter) / ((double) size * size);
 
         }
+    }
+
+    private void check(int N, int T)
+    {
+        if (N <= 0 || T <= 0)
+        { throw new IndexOutOfBoundsException("Row index i out of bounds"); }
     }
 
     // sample mean of percolation threshold
     public double mean()
     {
-
-        for (int i = 0; i < T; i++) {
-            mean += (threshold[i]/T);
+        for (int i = 0; i < numberOfExperiments; i++) {
+            mean += (threshold[i]/ numberOfExperiments);
         }
         return mean;
     }
@@ -59,48 +64,38 @@ public class PercolationStats
     // sample standard deviation of percolation threshold
     public double stddev()
     {
-        for (int i = 0; i < T; i++) {
-            stddev += ((threshold[i] - mean)*(threshold[i] - mean)/(T-1));
+        for (int i = 0; i < numberOfExperiments; i++) {
+            stddev += ((threshold[i] - mean)*(threshold[i] - mean)
+                    /(numberOfExperiments -1));
         }
-
         return Math.sqrt(stddev);
     }
 
     // returns lower bound of the 95% confidence interval
     public double confidenceLo()
     {
-        return (mean - (1.96*Math.sqrt(stddev))/Math.sqrt(T));
+        return (mean - (1.96*Math.sqrt(stddev))/Math.sqrt(numberOfExperiments));
     }
 
     // returns upper bound of the 95% confidence interval
     public double confidenceHi()
     {
-        return (mean + (1.96*Math.sqrt(stddev))/Math.sqrt(T));
+        return (mean + (1.96*Math.sqrt(stddev))/Math.sqrt(numberOfExperiments));
     }
 
     public static void main(String[] args) {
 
-//        if (args.length != 2)
-//            System.out.println("Arguments, please");
-//        System.out.println("Grid size: " + args[0]);
-//        System.out.println("Number of independent simulations: " + args[1]);
-//
-//
-//        PercolationStats test =
-//                new PercolationStats(Integer.parseInt(args[0]),
-// Integer.parseInt(args[1]));
-
-        PercolationStats test = new PercolationStats(200, 100);
+        Stopwatch stopwatch = new Stopwatch();
+        System.out.println("Grid size: " + args[0]);
+        System.out.println("Number of independent simulations: " + args[1]);
+        PercolationStats test =
+                new PercolationStats(Integer.parseInt(args[0]),
+                        Integer.parseInt(args[1]));
         System.out.println("mean = " + test.mean());
         System.out.println("stddev = " + test.stddev());
         System.out.println("95% confidence interval: "
                 + test.confidenceLo() + ", " + test.confidenceHi());
 
-        for (int i = 0; i < test.T; i++) {
-            System.out.println(test.threshold[i]);
-                }
-
-
-
+        System.out.println("Runtime: " + stopwatch.elapsedTime());
     }
 }
